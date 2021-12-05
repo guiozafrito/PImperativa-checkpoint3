a7e72e6ba6c5dc57f80fabaef3e6a833f9702006
guiozafrito <macschi@gmail.com>
2021-12-05 20:28:18 -0300

    one

---
+++ aluno.js
@@ -0,0 +1,13 @@
+class aluno {
+    Nome;
+    QtdDeFaltas;
+    Notas;
+    constructor(Nome, QtdDeFaltas, Notas) {
+        this.Nome= Nome;
+        this.QtdDeFaltas= QtdDeFaltas;
+        this.Notas= Notas;
+    }
+}
+//micro desafio passo 1
+
+module.exports = aluno;
