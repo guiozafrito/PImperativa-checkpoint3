dcd492c9a5b29453c8818cf9e3461aa87c4fe094
guiozafrito <macschi@gmail.com>
2021-12-10 00:46:26 -0300

    ok

--- aluno.js
+++ aluno.js
@@ -2,12 +2,11 @@ const aluno = {
     nome:'',
     QtdDeFaltas: 0,
     Notas: [],
-    constructor:(Nome, QtdDeFaltas, Notas) {
+    constructor:function(Nome, QtdDeFaltas, Notas) {
         this.Nome= Nome;
         this.QtdDeFaltas= QtdDeFaltas;
         this.Notas= Notas;
     },
-
 //micro desafio passo 1
 calcularMedia: function(aluno) {
     let total = 0;
@@ -16,7 +15,6 @@ calcularMedia: function(aluno) {
        total += nota; 
     });
     return total/aluno.notas.lenght;
-
 },
 aumentarFaltas: function(aluno){
     aluno.QtdDeFaltas++;
